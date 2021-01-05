aea1f69fa622083f1bf75f1464708fe38754c193
Mohit Yadav <mohit.yadav1991@gmail.com>
2021-01-05 15:03:16 +0530

    Initial commit

---
+++ src/test/java/com/scaler/uber/lite/UberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.uber.lite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
